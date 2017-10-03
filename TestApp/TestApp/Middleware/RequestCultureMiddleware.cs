bad68180820aa8ea27358541686b8deea74b0755
hanislovingit <hanislovingit@gmail.com>
2017-10-03 16:53:54 -0500

    playing with http pipeline in Startup class. Created a custom middleware.

---
+++ TestApp/TestApp/Middleware/RequestCultureMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace TestApp.Middleware
+{
+    public class RequestCultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestCultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var cultureQuery = context.Request.Query["culture"];
+            if (!string.IsNullOrWhiteSpace(cultureQuery))
+            {
+                var culture = new CultureInfo(cultureQuery);
+                CultureInfo.CurrentCulture = culture;
+                CultureInfo.CurrentUICulture = culture;
+            }
+            // call the next delegate middleware in the pipeline
+            return this._next(context);
+        }
+    }
+
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+    }
+}
--- TestApp/TestApp/Startup.cs
+++ TestApp/TestApp/Startup.cs
@@ -1,34 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TestApp.Middleware;
 
 namespace TestApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment hostingEnvironment, ILogger<Startup> logger, IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // add framework services
+            services.AddMvc();
+
+            // add app services
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            /* **********************************************************
+             * We can configure the http pipeline using Use, Run and Map.
+             * Use - can short-circuit the pipeline if it does not call a "next" request delegate.
+             * Run - terminates the pipeline
+             * Map - branches the pipeline
+             * MapWhen - branches the pipeline based on the result of the given predicate
+             ***********************************************************/
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                // todo
+                app.UseExceptionHandler("/Error/Error");
+            }
+
+            //app.UseAuthentication(); // todo: determine if we need this
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+
+            // custom middleware
+            app.UseRequestCulture();
+
+            // the first app.Run delegate terminates the request pipeline
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
         }
     }
 }
