bda0605392777f4b57821ff7b66a2df582bfb5dd
hanislovingit <hanislovingit@gmail.com>
2017-10-03 09:41:39 -0500

    Initial check in. This is the default code/template generated by VS 2017 when creating an asp.net core 2 MVC web project

---
+++ AspNetCore2_MvcTemplate/AspNetCore2_MvcTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore2_MvcTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
